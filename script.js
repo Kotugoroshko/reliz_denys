93ab3fb60f36d912c7a059c14f6498cafeb5a0cd
sooltthis15logiks <denzzik5@gmail.com>
2024-08-23 12:20:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 let team1 = document.querySelector('.team1')
 let team2 = document.querySelector('.team2')
 let team3 = document.querySelector('.team3')
-let height = Math.max(parseInt(getComputedStyle(team1).getPropertyValue('height')), parseInt(getComputedStyle(team2).getPropertyValue('height')), parseInt(getComputedStyle(team3).getPropertyValue('height')))
+let height = Math.max(parseInt(window.getComputedStyle(team1).getPropertyValue('height')), parseInt(window.getComputedStyle(team2).getPropertyValue('height')), parseInt(getComputedStyle(team3).getPropertyValue('height')))
 team1.style.height = `${height}px`
 team2.style.height = `${height}px`
 team3.style.height = `${height}px`
